5930359abc831eba4ab9ab4ca41887324b3a90e2
Ahmad Stanikzai <Shekibstanikzai.2016@gmail.com>
2023-01-12 09:58:51 -0800

    done

---
+++ Testing/Controllers/ProductCon.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controllers
+{
+    public class ProductCon : Controller
+    {
+        private readonly IProductRepo repo;
+
+        public ProductCon(IProductRepo repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
