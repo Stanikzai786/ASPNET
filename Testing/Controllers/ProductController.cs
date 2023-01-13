897382617e01b1c5e06e02b4365e3877e7b4bfa8
Ahmad Stanikzai <Shekibstanikzai.2016@gmail.com>
2023-01-13 11:39:22 -0800

    Done

---
+++ Testing/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepo repo;
+
+        public ProductController(IProductRepo repo)
+        {
+            this.repo = repo;
+        }
+        public IActionResult Index()
+        {
+            var allProducts = repo.GetAllProducts(); 
+            return View(allProducts);
+        }
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
+        public IActionResult UpdateProduct(int id)
+        {
+            Product prod = repo.GetProduct(id);
+            if (prod == null)
+            {
+                return View("ProductNotFound");
+            }
+            return View(prod);
+        }
+
+        public IActionResult UpdateProductToDatabase(Product product)
+        {
+            repo.UpdateProduct(product);
+
+            return RedirectToAction("ViewProduct", new { id = product.ProductID });
+        }
+    }
+}
--- Testing/IProductRepo.cs
+++ Testing/IProductRepo.cs
@@ -7,5 +7,7 @@ namespace Testing
     public interface IProductRepo
     {
         public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+         public void UpdateProduct(Product product);
     }
 }
